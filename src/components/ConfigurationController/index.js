729bdc159f274b7181f8572b6c8a6cd4cd2575bb
bhavani <bhavanithiruveedhula888@gmail.com>
2024-06-04 04:27:40 +0000

    Layout-Builder

--- src/components/ConfigurationController/index.js
+++ src/components/ConfigurationController/index.js
@@ -12,7 +12,7 @@ const ConfigurationController = () => (
         onToggleShowContent,
         onToggleShowLeftNavbar,
         onToggleShowRightNavbar,
-      } = value
+      } = value     
       const onClickContent = () => {
         onToggleShowContent()
       }
